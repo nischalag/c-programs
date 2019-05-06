385fdc570b06b7931894191cb1d218bfd8c88b34
nischalag <48782338+nischalag@users.noreply.github.com>
2019-05-06 12:56:02 +0530

    Create add2nos.c

---
+++ add2nos.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void getinput()
+{
+  int a,b;
+  printf("Enter the two numbers:");
+  scanf("%d%d",&a,&b);
+  add(a,b);
+}
+void add(int x, int y)
+{
+  int c;
+  c=x+y;
+  output(c);
+}
+void output(int sum)
+{
+  printf("sum of the two numbers=%d",sum);
+}
+main()
+{
+  getinput();
+}
